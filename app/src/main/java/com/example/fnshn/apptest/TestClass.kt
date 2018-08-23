be2b878fee0a9b59ffdd86ec6cfdc9adbf8ec770
Daniel <danielmaskell21@gmail.com>
2018-08-23 17:01:39 +1200

    Refactoring files

--- app/src/main/java/com/example/fnshn/apptest/TestClass.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fnshn.apptest
-
-class TestClass {
-    fun main (args: Array<String>) {
-        println("Hello, world!")
-        println("Suh dude")
-    }
-}
--- app/src/main/java/workitout/TestClass.kt
+++ app/src/main/java/workitout/TestClass.kt
@@ -1,4 +1,4 @@
-package apptest
+package workitout
 
 class TestClass {
 
