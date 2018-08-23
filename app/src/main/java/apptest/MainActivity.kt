bd839dc69d50c31ddae55f1a29680a26ad58e978
Daniel <danielmaskell21@gmail.com>
2018-08-23 16:47:15 +1200

    Test2

--- app/src/main/java/apptest/MainActivity.kt
+++ app/src/main/java/apptest/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.fnshn.apptest
+package apptest
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.fnshn.apptest.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
