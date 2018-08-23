25cb24584202679837442cd2fbf7b7b5e63977e5
Omri <omri.meizeles576@gmail.com>
2018-08-23 16:55:43 +1200

    testing

--- app/src/main/java/apptest/TestClass.kt
+++ app/src/main/java/apptest/TestClass.kt
@@ -1,5 +1,9 @@
 package apptest
 
 class TestClass {
-    
+
+    fun main(args: Array<String>) {
+
+        println("Push test")
+    }
 }
--- app/src/main/java/com/example/fnshn/apptest/TestClass.kt
+++ app/src/main/java/com/example/fnshn/apptest/TestClass.kt
@@ -3,5 +3,6 @@ package com.example.fnshn.apptest
 class TestClass {
     fun main (args: Array<String>) {
         println("Hello, world!")
+        println("Suh dude")
     }
 }
