66e51a70e59d4ea6e3f8fe341ea199de01d1d975
Daniel <danielmaskell21@gmail.com>
2018-08-28 13:27:10 +1200

    New code. It works on my phone.

--- app/src/main/java/workitout/workitout/User.kt
+++ app/src/main/java/workitout/workitout/User.kt
@@ -3,18 +3,78 @@ package workitout.workitout
 class User {
 
     private var userName = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var userPassword = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var userID = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var mondayAvail = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var tuesAvail = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var wedsAvail = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var thursAvail = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var friAvail = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var satAvail = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var sunAvail = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var homePhone = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var mobilePhone = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var emailAddress = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var homeAddress = null
+        get() = field
+        set(value) {
+            field = value
+        }
     private var contractType = null
+        get() = field
+        set(value) {
+            field = value
+        }
 }
