7d3c8187dca2886ad302fb797e52febb69eea896
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2018-08-30 16:20:23 +1200

    Added Role enumeration class for easier definition of employee status

---
+++ app/src/main/java/workitout/workitout/Role.kt
@@ -0,0 +1,11 @@
+package workitout.workitout
+
+enum class Role(val role: String) {
+    MANAGER("Manager"),
+    EMPLOYEE("Employee"),
+    SENIOR_EMPLOYEE("Senior Employee"),
+    EMPLOYER("Employer"),
+    TRAINEE("Trainee")
+
+    
+}
