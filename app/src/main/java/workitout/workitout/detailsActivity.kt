284831263a59202f7d337f93a772551752b44c2a
Omri <omri.meizeles576@gmail.com>
2018-09-18 12:47:31 +1200

    Details button in menu works

--- app/src/main/java/workitout/workitout/detailsActivity.kt
+++ app/src/main/java/workitout/workitout/detailsActivity.kt
@@ -1,21 +1,25 @@
 package workitout.workitout
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_the_menu.*
 
-class theMenu : AppCompatActivity() {
+class detailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_the_menu)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
 
+        details.setOnClickListener{
+            val detailsActivity = Intent(this@detailsActivity, detailsActivity::class.java)
+            startActivity (detailsActivity)
+        }
     }
 }
