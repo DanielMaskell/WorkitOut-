25e1a235a1df5d544450aaba9554e4c4e2ea52a4
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2018-09-22 20:39:37 +1200

    Created DatabaseHandler and modified UserDetails

--- app/src/main/java/workitout/workitout/UserDetails.kt
+++ app/src/main/java/workitout/workitout/UserDetails.kt
@@ -1,80 +1,8 @@
 package workitout.workitout
 
-class UserDetails {
+class   UserDetails(employeeID: String?, fName: String?, lName: String?, password: String?,
+                    mondayAvail: String?, tuesdayAvail: String?, wednesdayAvail: String?, thursdayAvail: String?,
+                    fridayAvail: String?, saturdayAvail: String?, sundayAvail: String?, homePhone: String?,
+                    mobilePhone: String?, homeAddress: String?, emailAddress: String?, role: Role?) {
 
-    private var userName = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var userPassword = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var userID = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var mondayAvail = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var tuesAvail = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var wedsAvail = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var thursAvail = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var friAvail = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var satAvail = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var sunAvail = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var homePhone = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var mobilePhone = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var emailAddress = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var homeAddress = null
-        get() = field
-        set(value) {
-            field = value
-        }
-    private var contractType = null
-        get() = field
-        set(value) {
-            field = value
-        }
 }
