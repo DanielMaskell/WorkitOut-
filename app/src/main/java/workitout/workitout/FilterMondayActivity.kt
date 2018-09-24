d95e69ab1dcee4bcb9757156e28b1afdc1b50688
weiminxu <42362638+weiminxu@users.noreply.github.com>
2018-09-25 10:38:04 +1200

    As an emploee, i would like to have a access to a filtered list of shifts that i will be working. there are six activities for this kind of user story

---
+++ app/src/main/java/workitout/workitout/FilterMondayActivity.kt
@@ -0,0 +1,15 @@
+package workitout.workitout
+
+import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_the_menu.*
+
+class FilterMondayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mymondaywork)
+    }
+}
---
+++ app/src/main/java/workitout/workitout/FilterMyTursdayActivity.kt
@@ -0,0 +1,12 @@
+package workitout.workitout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FilterMyTursdayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mythursdaywork)
+    }
+}
